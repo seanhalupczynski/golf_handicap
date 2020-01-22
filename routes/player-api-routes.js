80e75f1b3085276693b7e5f839120be968c17771
Sean Halupczynski <sean.halup@gmail.com>
2020-01-21 21:29:04 -0500

    Created CRD for player api
    created GET /api/players to return all players on player table
    
    created GET /api/players/:id route to return individual player data
    
    created POST /api/players route to add a player to player table
    
    create DELETE /api/players/:id route to delete player from player table

--- routes/player-api-routes.js
+++ routes/player-api-routes.js
@@ -1,5 +1,44 @@
 var db = require("../models")
 
 module.exports = function(app){
+// ========== GET ROUTES ==========
+    // Get all the players info from Player table
+    app.get("/api/players", function(req, res){
+        db.Player.findAll().then(function(playerRes){
+            res.json(playerRes);
+        });
+    });
 
+    // Get individual player info from Player table by id
+    app.get("/api/players/:id", function(req, res){
+        db.Player.findAll({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(playerRes){
+            res.json(playerRes);
+        });
+    });
+
+// ========== POST ROUTES ==========
+    // Add a new player to the Player table
+    app.post("/api/players", function(req, res){
+        db.Player.create(req.body).then(function(playerRes){
+            res.json(playerRes);
+        });
+    });
+
+// ========== PUT ROUTES ==========
+
+// ========== DELETE ROUTES ==========
+    // Delete a player from Player table by id
+    app.delete("/api/players/:id", function(req, res){
+        db.Player.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(playerRes){
+            res.json(playerRes);
+        });
+    });
 };
