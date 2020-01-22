17dabb7262fd6677301df80579b6d2d078c28b42
Sean Halupczynski <sean.halup@gmail.com>
2020-01-21 21:38:46 -0500

    Created CRD routes for golf courses
    GET /api/golf_courses returns all golf course data
    
    GET /api/golf_courses/:id returns all data for specific golf course
    
    POST /api/golf_courses creates a new golf course record
    
    DELETE /api/golf_courses/:id removes a golf course from the DB by id

--- routes/golf-course-api-routes.js
+++ routes/golf-course-api-routes.js
@@ -1,5 +1,44 @@
 var db = require("../models")
 
 module.exports = function(app){
+// ========== GET ROUTES ==========
+    // Get all the golf courses info from GolfCourse table
+    app.get("/api/golf_courses", function(req, res){
+        db.GolfCourse.findAll().then(function(golfCourseRes){
+            res.json(golfCourseRes);
+        });
+    });
 
+    // Get individual golf course info from GolfCourse table by id
+    app.get("/api/golf_courses/:id", function(req, res){
+        db.GolfCourse.findAll({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(golfCourseRes){
+            res.json(golfCourseRes);
+        });
+    });
+
+// ========== POST ROUTES ==========
+    // Add a new golf course to the GolfCourse table
+    app.post("/api/golf_courses", function(req, res){
+        db.GolfCourse.create(req.body).then(function(golfCourseRes){
+            res.json(golfCourseRes);
+        });
+    });
+
+// ========== PUT ROUTES ==========
+
+// ========== DELETE ROUTES ==========
+    // Delete a golf course from GolfCourse table by id
+    app.delete("/api/golf_courses/:id", function(req, res){
+        db.GolfCourse.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(golfCourseRes){
+            res.json(golfCourseRes);
+        });
+    });
 };
