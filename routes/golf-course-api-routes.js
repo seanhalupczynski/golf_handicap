bb987c75e9b0b3de6b8b3ac2724f8182fa5612b0
Halupczynski <Sean.Halupczynski@LibertyMutual.com>
2020-01-23 12:33:25 -0500

    CRD routes for golfcourse R routes for golfround
    created some Create Read and Delete routes for the golf course api
    
    creates some Read routes for the golf-round api

--- routes/golf-course-api-routes.js
+++ routes/golf-course-api-routes.js
@@ -1,5 +1,44 @@
 var db = require("../models")
 
 module.exports = function(app){
+// ========== GET ROUTES ==========
+    // Get all golf courses from GolfCourse table
+    app.get("/api/GolfCourses", function(req, res){
+        db.GolfCourse.findAll().then(function(golfCourseRes){
+            res.json(golfCourseRes);
+        });
+    });
 
+    // Get specific golf course from GolfCourse table by id
+    app.get("/api/GolfCourses/:id", function(req, res){
+        db.GolfCourse.findAll({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(golfCourseRes){
+            res.json(golfCourseRes);
+        });
+    });
+    
+// ========== POST ROUTES ==========
+    // Add a golf course to the Golf Course table
+    app.post("/api/GolfCourses", function(req, res){
+        db.GolfCourse.create(req.body).then(function(golfCourseRes){
+            res.json(golfCourseRes);
+        });
+    });
+// ========== PUT ROUTES ==========
+    // Update a golf course's data on the Golf Course table
+
+// ========== DELETE ROUTES ==========
+    // Delete a golf course from the Golf Course table
+    app.delete("api/GolfCourse/:id", function(req, res){
+        db.GolfCourse.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(golfCourseRes){
+            res.json(golfCourseRes);
+        });
+    });
 };
--- routes/golf-round-api-routes.js
+++ routes/golf-round-api-routes.js
@@ -2,8 +2,24 @@ var db = require("../models")
 
 module.exports = function(app){
 // ========== GET ROUTES ==========
-    // Get all rounds
-    
+    // Get all golf rounds on the GolfRound table
+    app.get("/api/GolfRounds", function(req, res){
+        db.GolfRound.findAll().then(function(golfRoundRes){
+            res.json(golfRoundRes);
+        });
+    });
+
+    // Get specific golf round from the GolfRound table by id
+    app.get("/api/GolfRounds/:id", function(req, res){
+        db.GolfRound.findAll({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(golfRoundRes){
+            res.json(golfRoundRes);
+        });
+    });
+
 // ========== POST ROUTES ==========
 
 // ========== PUT ROUTES ==========
