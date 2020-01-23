30d0a7642d681d5dc659e887f2a07eecddacd47a
Halupczynski <Sean.Halupczynski@LibertyMutual.com>
2020-01-23 10:17:58 -0500

    add 2 fields to player table
    added a username filed that is a string and can't be null
    
    added a password field that is a string and can't be null
    
    added passport to dependencies

--- routes/golf-round-api-routes.js
+++ routes/golf-round-api-routes.js
@@ -1,5 +1,13 @@
 var db = require("../models")
 
 module.exports = function(app){
+// ========== GET ROUTES ==========
+    // Get all rounds
+    
+// ========== POST ROUTES ==========
+
+// ========== PUT ROUTES ==========
+
+// ========== DELETE ROUTES ==========
 
 };
