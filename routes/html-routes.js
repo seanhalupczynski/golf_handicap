9596051eba5bc1ed120b1ee3060325b4195c529b
Halupczynski <Sean.Halupczynski@LibertyMutual.com>
2020-01-24 12:32:32 -0500

    started html routes
    created route for the homepage

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,6 +11,9 @@ var path = require("path");
 module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
+  app.get("/", function(req, res){
+    res.send(path.join(__dirname + "../public/index.html"))
+  });
 
 
 };
