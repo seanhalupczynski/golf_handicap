262530a55e6e07308dee44c18229a5c6a900db7e
Sean Halupczynski <sean.halup@gmail.com>
2020-01-25 13:48:03 -0500

    UI layout started, added some custom css, added fields to GolfRound Model
    Added some basic layout stuff for homepage, player page, and golf course page
    
    Added styling to have fixed header and footer
    
    Added styling to move menu button to the right
    
    Added fields to GolfRound model to track front and back nine scores and stats and total round stats

--- models/GolfRound.js
+++ models/GolfRound.js
@@ -1,8 +1,68 @@
 module.exports = function(sequelize, DataTypes){
     var GolfRound = sequelize.define("GolfRound", {
-        score: {
+        front_score: {
+            type: DataTypes.INTEGER
+        },
+        front_putts: {
+            type: DataTypes.INTEGER
+        },
+        front_firs: {
+            type: DataTypes.INTEGER
+        },
+        front_girs: {
+            type: DataTypes.INTEGER
+        },
+        front_sand_saves: {
+            type: DataTypes.INTEGER
+        },
+        front_up_and_down: {
+            type: DataTypes.INTEGER
+        },
+        front_penalty_strokes: {
+            type: DataTypes.INTEGER
+        },
+        back_score: {
+            type: DataTypes.INTEGER
+        },
+        back_putts: {
+            type: DataTypes.INTEGER
+        },
+        back_firs: {
+            type: DataTypes.INTEGER
+        },
+        back_girs: {
+            type: DataTypes.INTEGER
+        },
+        back_sand_saves: {
+            type: DataTypes.INTEGER
+        },
+        back_up_and_down: {
+            type: DataTypes.INTEGER
+        },
+        back_penalty_strokes: {
+            type: DataTypes.INTEGER
+        },
+        total_score: {
             type: DataTypes.INTEGER,
             allowNull: false
+        },
+        total_putts: {
+            type: DataTypes.INTEGER
+        },
+        total_firs: {
+            type: DataTypes.INTEGER
+        },
+        total_girs: {
+            type: DataTypes.INTEGER
+        },
+        total_sand_saves: {
+            type: DataTypes.INTEGER
+        },
+        total_up_and_down: {
+            type: DataTypes.INTEGER
+        },
+        total_penalty_strokes: {
+            type: DataTypes.INTEGER
         },
         date_played: {
             type: DataTypes.DATEONLY
